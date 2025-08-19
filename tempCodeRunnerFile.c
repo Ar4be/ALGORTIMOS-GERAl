c5e23335a85b574708cc82813310ede84cfd9147
Ar4be <itsmiro61@gmail.com>
2025-08-18 21:38:56 -0300

    MInhas tarefas do dia

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int valor_a;
+    int valor_b;
+    int valor_c;
+    int soma
+
+//saida do valor
+printf ("digite o valor a");
+scanf ("&d",&valor_a);
+
+printf ("digite o valor b");
+scanf ("%d", &valor_b);
+
+printf ("digite o valor c");
+scanf ("%d", &valor_c);
+return 0;
+
+}
